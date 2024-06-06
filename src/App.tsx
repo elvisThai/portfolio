cda65de5a3eb68912be15593b5048470e4654abc
Elvis Thai <elvisthai246@gmail.com>
2024-06-05 21:41:57 -0400

    Updated portfolio with new content and features

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,128 @@ import React from 'react';
 import './App.css';
 import { Reveal } from './Reveal'; 
 import Cursor from './Cursor';
-import NavList from './HoverEffect';
+import NavList from './Navigation';
+import { Stars } from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
+
 
 function App() {
   return (
     <div className="app">
+
+      {/* stars effect */}
+      <div className = "stars">
+        <Canvas>
+          <Stars radius ={50} count = {1500} factor = {3} fade speed = {2}></Stars>
+        </Canvas>
+      </div>
 
       {/* cursor component effect*/}
       <Cursor></Cursor>
 
       {/*introduction text surrounded with reveal component*/}
       <div className = "intro">
-      <Reveal>
+    
       <h3>Hi, my name is</h3>
-      </Reveal>
-      <Reveal>
-      <h2>Elvis Thai.</h2>
-      </Reveal>
-      <Reveal>
-      <p>I am a computer science student at Western University expected to graduate in May 2027. I am currently seeking opportunities to learn!</p>
-      </Reveal>
+     
+      <h2>Elvis Thai</h2>
+    
+      <p>I am a computer science student at Western University expected to graduate in May 2027. I am currently seeking opportunities to apply my skills and learn!</p>
+      
       </div>
 
       {/*navigation bar component*/}
-     
-      <NavList />
-     
+      <NavList></NavList>
 
+      {/*about section*/}
+      <div className = "heading about">
+      <Reveal>
+      <h1>about me</h1>
+      </Reveal>
+      <Reveal>
+      <p className = "aboutParagraph">As a computer science student I thrive on solving complex problems and am always eager to learn new technologies! 
+      I have a passion for software engineering in both front-end and back-end development, with a keen interest in artificial intelligence and data science.
+      <br></br><br></br>To further my knowledge and skills, I have built and am building full stack projects that align with my interests and passions. These projects include developing 
+      pixel-perfect web applications, implementing machine learning models, and analyzing data sets to extract meaningful insights. 
+      <br></br><br></br>When I am not coding, I am probably running, rock climbing, or gaming!
+      </p>
+      </Reveal>
+      </div>
+
+      {/*logos*/}
+      <div className = "logos">
+      <a href="https://www.linkedin.com/in/elvisthai/" target="_blank">
+        
+      <img className = "linkedin" src="src\images\linkedinLogo.png" alt="linkedinLogo"></img>
+      </a>
+      
+      <a href="https://github.com/elvisThai" target="_blank">
+      <img className = "github" src="src\images\github.png" alt="linkedinLogo"></img>
+      </a>
+  
+      </div>
+
+      {/*skills section*/}
+      <div className = "heading skills">
+      <Reveal>
+      <h1 id = "skills"><br></br><br></br><br></br><br></br><br></br><br></br>skills</h1>
+      </Reveal>
+      <Reveal>
+      <p className = "skillCategory">LANGUAGES &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; FRAMEWORKS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; TOOLS</p>
+      </Reveal>
+      <Reveal>
+        <ul className = "skillList">
+          <li>Python</li>
+          <li>Java</li>
+          <li>Javascript</li>
+          <li>Typescript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SQL</li>
+        </ul>
+      </Reveal>
+      <Reveal>
+        <ul className = "skillList frameworks">
+          <li>React</li>
+          <li>Node.js</li>
+        </ul></Reveal>
     
+        <Reveal>
+        <ul className = "skillList tools">
+          <li>Git&nbsp;&&nbsp;Github</li>
+          <li>VS&nbsp;Code</li>
+          <li>PyCharm</li>
+          <li>IntelliJ</li>
+          <li>Eclipse</li>
+        </ul></Reveal>
+
+      </div>
+
+      {/*projects section*/}
+      <div className = "heading projects">
+      <Reveal>
+      <h1 id = "projects"><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>projects</h1>
+      </Reveal>
+      <Reveal>
+      <p className = "projectsParagraph">coming soon</p>
+      </Reveal>
+      </div>
+
+       {/*contact section*/}
+      <div className = "heading">
+      <Reveal>
+      <h1 id = "contact"><br></br><br></br><br></br><br></br><br></br><br></br>contact</h1>
+      </Reveal>
+     
+      <Reveal>
+      <a className="email contact" href="mailto:elvisthai246@gmail.com">elvis.thai@outlook.com</a>
+      </Reveal>
+      <Reveal>
+      <h3 className = "contact">416-662-1097</h3>
+      </Reveal>
+      
+
+      </div>
 
     </div>
   );
---
+++ src/Navigation.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Reveal } from './Reveal'; 
+
+const NavList = () => {
+  return (
+    <ul className="nav-list" style={{right: "80.5%", top: "50%", position: "fixed" }}>
+      <li style={{listStyle: 'none'}}>
+        <a
+          href="#"
+          style={{ marginLeft: '20%', marginTop: '5%', lineHeight: '50%', color: '#D3D3D3', textDecoration: 'none', fontFamily: '"Reddit Sans", sans-serif', fontSize: '130%' }}
+        >
+          <motion.span
+           whileHover={{ scale: 1.2, x: '20%' }}
+           style={{ display: 'inline-block', transition: 'margin-left 0.1s linear'}}
+          >
+            <p>About</p>
+          </motion.span>
+        </a>
+      </li>
+      <li style={{listStyle: 'none' }}>
+        <a
+          href="#skills"
+          style={{ marginLeft: '20%', marginTop: '5%', lineHeight: '50%', color: '#D3D3D3', textDecoration: 'none', fontFamily: '"Reddit Sans", sans-serif', fontSize: '130%' }}
+        >
+          <motion.span
+            whileHover={{ scale: 1.2, x: '20%' }}
+            style={{ display: 'inline-block', transition: 'margin-left 0.1s linear'}}
+          >
+            <p>Skills</p>
+          </motion.span>
+        </a>
+      </li>
+      <li style={{listStyle: 'none' }}>
+        <a
+          href="#projects"
+          style={{ marginLeft: '20%', marginTop: '5%', lineHeight: '50%', color: '#D3D3D3', textDecoration: 'none', fontFamily: '"Reddit Sans", sans-serif', fontSize: '130%' }}
+        >
+          <motion.span
+            whileHover={{ scale: 1.2, x: '20%' }}
+            style={{ display: 'inline-block', transition: 'margin-left 0.1s linear'}}
+          >
+            <p>Projects</p>
+          </motion.span>
+        </a>
+      </li>
+      <li style={{listStyle: 'none' }}>
+        <a
+          href="#contact"
+          style={{ marginLeft: '20%', marginTop: '5%', lineHeight: '50%', color: '#D3D3D3', textDecoration: 'none', fontFamily: '"Reddit Sans", sans-serif', fontSize: '130%' }}
+        >
+          <motion.span
+            whileHover={{ scale: 1.2, x: '20%' }}
+            style={{ display: 'inline-block', transition: 'margin-left 0.1s linear'}} 
+          >
+            <p>Contact</p>
+          </motion.span>
+        </a>
+      </li>
+    </ul>
+  )
+}
+
+export default NavList;
